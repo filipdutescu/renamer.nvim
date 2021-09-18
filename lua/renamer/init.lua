645bf09520d089d6912d7a48885cd1832ece5dad
Filip Dutescu <filip.dutescu@gmail.com>
2021-09-18 13:34:42 +0300

    fix: window close; feat: window style

--- lua/renamer/init.lua
+++ lua/renamer/init.lua
@@ -34,7 +34,7 @@ function renamer.rename(word)
         title = renamer.title,
         padding = renamer.padding,
         border = renamer.border,
-        border_chars = renamer.border_chars,
+        borderchars = renamer.border_chars,
         width = #renamer.title + 4,
         line = line + 1,
         col = word_start + math.floor((word_end - word_start) / 2),
@@ -43,47 +43,31 @@ function renamer.rename(word)
         callback = function() print('Hello World!') end,
     }
     local prompt_win_id, prompt_opts = popup.create(cword, popup_opts)
-    local prompt_buf_id = vim.api.nvim_win_get_buf(prompt_win_id)
     renamer._buffers[prompt_win_id] = prompt_opts
 
-    vim.api.nvim_win_set_option(prompt_win_id, 'wrap', false)
-    vim.api.nvim_win_set_option(prompt_win_id, 'winblend', 0)
-    if renamer.prefix ~= '' then
-        vim.api.nvim_buf_set_option(prompt_win_id, 'buftype', 'prompt')
-        vim.fn.prompt_setprompt(prompt_buf_id, renamer.prefix)
-    end
-
-    local on_leave = string.format(
-        [[  autocmd BufLeave,WinLeave,InsertLeave <buffer> ++nested ++once :silent lua require'renamer'.on_close(%s)]],
-        prompt_win_id
-    )
-    vim.cmd [[startinsert]]
-    vim.cmd [[augroup RenamerInsert]]
-    vim.cmd [[  au!]]
-    vim.cmd(on_leave)
-    vim.cmd [[augroup end]]
+    renamer._setup_window(prompt_win_id)
+    renamer._set_prompt_win_style(prompt_win_id)
 end
 
 function renamer.on_close(window_id)
     local delete_window = function(win_id)
-        if (not win_id == nil)
-            and vim.api.nvim_win_is_valid(win_id)
-            and renamer._buffers
-            and renamer._buffers[win_id] then
+        if win_id and vim.api.nvim_win_is_valid(win_id) then
             local buf_id = vim.api.nvim_win_get_buf(win_id)
             if vim.api.nvim_buf_is_valid(buf_id) and not vim.api.nvim_buf_get_option(buf_id, 'buflisted') then
                 vim.cmd(string.format('silent! bdelete! %s', buf_id))
             end
 
             if vim.api.nvim_win_is_valid(win_id) then
+                vim.api.nvim_win_close(win_id, true)
                 if not pcall(vim.api.nvim_win_close, win_id, true) then
-                    log.trace('Failed to close window: rename_prompt_win/' .. win_id)
+                   log.trace('Failed to close window: rename_prompt_win/' .. win_id)
                 end
             end
         end
 
-        if (not win_id == nil) and renamer._buffers and renamer._buffers[win_id] then
+        if win_id and renamer._buffers and renamer._buffers[win_id] then
             renamer._buffers[win_id] = nil
+            renamer._delete_autocmds()
         end
     end
 
@@ -91,7 +75,6 @@ function renamer.on_close(window_id)
     local border_win_id = opts and opts.border and opts.border.win_id
     delete_window(window_id)
     delete_window(border_win_id)
-    --vim.defer_fn(function() delete_window(window_no) end, 10)
 end
 
 function renamer._get_cursor()
@@ -115,6 +98,58 @@ function renamer._get_word_boundaries_in_line(line, word, line_pos)
     end
 
     return word_start, word_end
+end
+
+function renamer._setup_window(prompt_win_id)
+    local prompt_buf_id = vim.api.nvim_win_get_buf(prompt_win_id)
+
+    vim.api.nvim_win_set_option(prompt_win_id, 'wrap', false)
+    vim.api.nvim_win_set_option(prompt_win_id, 'winblend', 0)
+    if renamer.prefix ~= '' then
+        vim.api.nvim_buf_set_option(prompt_win_id, 'buftype', 'prompt')
+        vim.fn.prompt_setprompt(prompt_buf_id, renamer.prefix)
+    end
+
+    vim.cmd [[startinsert]]
+    renamer._create_autocmds(prompt_win_id)
+end
+
+function renamer._set_prompt_win_style(prompt_win_id)
+    if prompt_win_id then
+        vim.api.nvim_win_set_option(prompt_win_id, 'winhl', 'Normal:RenamerNormal')
+        vim.api.nvim_win_set_option(prompt_win_id, 'winblend', 0)
+
+        if renamer._buffers and renamer._buffers[prompt_win_id] then
+            local opts = renamer._buffers[prompt_win_id]
+            local border_win_id = opts.border and opts.border.win_id
+
+            renamer._set_prompt_border_win_style(border_win_id)
+        end
+    end
+end
+
+function renamer._set_prompt_border_win_style(prompt_border_win_id)
+   if prompt_border_win_id then
+        vim.api.nvim_win_set_option(prompt_border_win_id, 'winhl', 'Normal:RenamerNormal')
+        vim.api.nvim_win_set_option(prompt_border_win_id, 'winblend', 0)
+   end
+end
+
+function renamer._create_autocmds(prompt_win_id)
+    local on_leave = string.format(
+        [[  autocmd BufLeave,WinLeave,InsertLeave <buffer> ++nested ++once :silent lua require'renamer'.on_close(%s)]],
+        prompt_win_id
+    )
+    vim.cmd [[augroup RenamerInsert]]
+    vim.cmd [[  au!]]
+    vim.cmd(on_leave)
+    vim.cmd [[augroup end]]
+end
+
+function renamer._delete_autocmds()
+    vim.cmd [[augroup RenamerInsert]]
+    vim.cmd [[  au!]]
+    vim.cmd [[augroup end]]
 end
 
 return renamer
