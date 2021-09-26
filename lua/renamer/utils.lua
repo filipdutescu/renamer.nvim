8a4745fd41e4f4a94aa259ca9e67f5517914e41b
Filip Dutescu <filip.dutescu@gmail.com>
2021-09-26 17:48:15 +0300

    chore: Apply StyLua styles

--- lua/renamer/utils.lua
+++ lua/renamer/utils.lua
@@ -5,7 +5,7 @@ function utils.get_value_or_default(table, name, default)
         local actual_type, expected_type = type(table[name]), type(default)
         assert(
             type(table[name]) == type(default),
-            string.format('Invalid type for \'%s\'. Expected \'%s\', but found \'%s\'.', name, actual_type, expected_type)
+            string.format('Invalid type for "%s". Expected "%s", but found "%s".', name, actual_type, expected_type)
         )
         return table[name]
     else
