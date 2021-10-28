f9bafe66c00780c34a8d86e035525cb6cb77bb2a
Filip Dutescu <filip.dutescu@gmail.com>
2021-10-28 23:03:25 +0300

    fix(keymaps): all keymaps put the user in insert mode
    Since all keymaps do the equivalent of typing the `normal` mode keymaps
    for movement, switch `nvim_feedkeys()` with `nvim_input()`, which has
    less (confusing) options and does exactly what typing the commands
    should.
    
    Due to neither function making it possible to go back to `insert`
    mode using `nvim_command('startinsert')`, all keymaps which did not put
    the buffer in `insert` mode have an `i` appended to them.
    
    Fixes: GH-31
    Signed-off-by: Filip Dutescu <filip.dutescu@gmail.com>

--- lua/renamer/mappings/utils.lua
+++ lua/renamer/mappings/utils.lua
@@ -1,45 +1,36 @@
 local utils = {}
 
-utils.exec_in_normal = function(callback, opts, ...)
-    vim.cmd [[stopinsert]]
+utils.exec_in_normal = function(callback, ...)
+    vim.api.nvim_command [[stopinsert]]
     callback(...)
-
-    if opts and opts.keep_insert then
-        vim.api.nvim_feedkeys('i', 'n', true)
-    end
 end
 
 utils.set_cursor_to_end = function()
-    utils.exec_in_normal(vim.api.nvim_feedkeys, {}, 'A', 'n', true)
+    utils.exec_in_normal(vim.api.nvim_input, 'A')
 end
 
 utils.set_cursor_to_start = function()
-    utils.exec_in_normal(vim.api.nvim_feedkeys, {}, 'I', 'n', true)
+    utils.exec_in_normal(vim.api.nvim_input, 'I')
 end
 
 utils.set_cursor_to_word_end = function()
-    utils.exec_in_normal(vim.api.nvim_feedkeys, { keep_insert = true }, 'e', 'n', true)
+    utils.exec_in_normal(vim.api.nvim_input, 'ei')
 end
 
 utils.set_cursor_to_word_start = function()
-    utils.exec_in_normal(vim.api.nvim_feedkeys, { keep_insert = true }, 'b', 'n', true)
+    utils.exec_in_normal(vim.api.nvim_input, 'bi')
 end
 
 utils.clear_line = function()
-    utils.exec_in_normal(vim.api.nvim_feedkeys, {}, '0C', 'n', true)
+    utils.exec_in_normal(vim.api.nvim_input, '0C')
 end
 
 utils.undo = function()
-    utils.exec_in_normal(vim.api.nvim_feedkeys, { keep_insert = true }, 'u', 't', true)
+    utils.exec_in_normal(vim.api.nvim_input, 'ui')
 end
 
 utils.redo = function()
-    utils.exec_in_normal(function()
-        local key = vim.api.nvim_replace_termcodes('<c-r>', true, false, true)
-        vim.api.nvim_feedkeys(key, 't', true)
-    end, {
-        keep_insert = true,
-    })
+    utils.exec_in_normal(vim.api.nvim_input, '<c-r>i')
 end
 
 return utils
--- lua/tests/mappings/utils_spec.lua
+++ lua/tests/mappings/utils_spec.lua
@@ -7,35 +7,17 @@ local eq = assert.are.same
 describe('mappings', function()
     describe('utils', function()
         describe('exec_in_normal', function()
-            it('should exit "insert" mode and execute callback (and enter "insert" mode afterwards)', function()
-                local was_called = false
-                local cmd = stub(vim, 'cmd')
-                local nvim_feedkeys = stub(vim.api, 'nvim_feedkeys')
-
-                utils.exec_in_normal(function()
-                    was_called = true
-                end, {
-                    keep_insert = true,
-                })
-
-                eq(true, was_called)
-                assert.spy(cmd).was_called_with [[stopinsert]]
-                assert.spy(nvim_feedkeys).was_called_with('i', 'n', true)
-            end)
-
             it('should exit "insert" mode and execute callback', function()
                 local was_called = false
-                local cmd = stub(vim, 'cmd')
+                local nvim_command = stub(vim.api, 'nvim_command')
 
                 utils.exec_in_normal(function()
                     was_called = true
-                end, {
-                    keep_insert = false,
-                })
+                end)
 
                 eq(true, was_called)
-                assert.spy(cmd).was_called_with [[stopinsert]]
-                assert.spy(cmd).called_at_most(1)
+                assert.spy(nvim_command).was_called_with [[stopinsert]]
+                assert.spy(nvim_command).called_at_most(1)
             end)
 
             it('should execute callback with arguments', function()
@@ -43,80 +25,79 @@ describe('mappings', function()
 
                 utils.exec_in_normal(function(val)
                     was_called = val
-                end, {}, true)
+                end, true)
 
                 eq(true, was_called)
             end)
         end)
 
         describe('set_cursor_to_end', function()
-            it('should call `vim.api.nvim_feedkeys` with "A"', function()
-                local nvim_feedkeys = stub(vim.api, 'nvim_feedkeys')
+            it('should call `vim.api.nvim_input` with "A"', function()
+                local nvim_input = stub(vim.api, 'nvim_input')
 
                 utils.set_cursor_to_end()
 
-                assert.spy(nvim_feedkeys).was_called_with('A', 'n', true)
+                assert.spy(nvim_input).was_called_with 'A'
             end)
         end)
 
         describe('set_cursor_to_start', function()
-            it('should call `vim.api.nvim_feedkeys` with "I"', function()
-                local nvim_feedkeys = stub(vim.api, 'nvim_feedkeys')
+            it('should call `vim.api.nvim_input` with "I"', function()
+                local nvim_input = stub(vim.api, 'nvim_input')
 
                 utils.set_cursor_to_start()
 
-                assert.spy(nvim_feedkeys).was_called_with('I', 'n', true)
+                assert.spy(nvim_input).was_called_with 'I'
             end)
         end)
 
         describe('set_cursor_to_word_end', function()
-            it('should call `vim.api.nvim_feedkeys` with "e"', function()
-                local nvim_feedkeys = stub(vim.api, 'nvim_feedkeys')
+            it('should call `vim.api.nvim_input` with "e"', function()
+                local nvim_input = stub(vim.api, 'nvim_input')
 
                 utils.set_cursor_to_word_end()
 
-                assert.spy(nvim_feedkeys).was_called_with('e', 'n', true)
+                assert.spy(nvim_input).was_called_with 'ei'
             end)
         end)
 
         describe('set_cursor_to_word_start', function()
-            it('should call `vim.api.nvim_feedkeys` with "b"', function()
-                local nvim_feedkeys = stub(vim.api, 'nvim_feedkeys')
+            it('should call `vim.api.nvim_input` with "bi"', function()
+                local nvim_input = stub(vim.api, 'nvim_input')
 
                 utils.set_cursor_to_word_start()
 
-                assert.spy(nvim_feedkeys).was_called_with('b', 'n', true)
+                assert.spy(nvim_input).was_called_with 'bi'
             end)
         end)
 
         describe('clear_line', function()
-            it('should call `vim.api.nvim_feedkeys` with "0C"', function()
-                local nvim_feedkeys = stub(vim.api, 'nvim_feedkeys')
+            it('should call `vim.api.nvim_input` with "0C"', function()
+                local nvim_input = stub(vim.api, 'nvim_input')
 
                 utils.clear_line()
 
-                assert.spy(nvim_feedkeys).was_called_with('0C', 'n', true)
+                assert.spy(nvim_input).was_called_with '0C'
             end)
         end)
 
         describe('undo', function()
-            it('should call `vim.api.nvim_feedkeys` with "u"', function()
-                local nvim_feedkeys = stub(vim.api, 'nvim_feedkeys')
+            it('should call `vim.api.nvim_input` with "ui"', function()
+                local nvim_input = stub(vim.api, 'nvim_input')
 
                 utils.undo()
 
-                assert.spy(nvim_feedkeys).was_called_with('u', 't', true)
+                assert.spy(nvim_input).was_called_with 'ui'
             end)
         end)
 
         describe('redo', function()
-            it('should call `vim.api.nvim_feedkeys` with "<c-r>"', function()
-                local key = vim.api.nvim_replace_termcodes('<c-r>', true, false, true)
-                local nvim_feedkeys = stub(vim.api, 'nvim_feedkeys')
+            it('should call `vim.api.nvim_input` with "<c-r>i"', function()
+                local nvim_input = stub(vim.api, 'nvim_input')
 
                 utils.redo()
 
-                assert.spy(nvim_feedkeys).was_called_with(key, 't', true)
+                assert.spy(nvim_input).was_called_with '<c-r>i'
             end)
         end)
     end)
