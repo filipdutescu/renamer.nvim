filipdutescu/renamer.nvim

commit cdb40c61a3460a4bd879d6aa03e06b03083a653a

fix: set Neovim mode to initial one
Due to various refactoring which did not take into account multiple
occurences of the same word, the initial mode was not accounted for when
closing the buffer. This commit addresses the issue as follows:

- the logic for returning Noevim to the mode in which
  `renamer.rename()` was called in is moved to `renamer.on_close()`
- use the proper fields to get the initial mode from `renamer._buffers`

Signed-off-by: Filip Dutescu <filip.dutescu@gmail.com>

lua/renamer/init.lua
@@ -131,16 +131,10 @@ function renamer.on_submit(window_id)
     if window_id and renamer._buffers and renamer._buffers[window_id] then
         local buf_id = vim.api.nvim_win_get_buf(window_id)
         local new_word = vim.api.nvim_buf_get_lines(buf_id, -2, -1, false)[1]
-
         log.fmt_info('Submitted word: "%s".', new_word)
 
         renamer._delete_autocmds()
-        local initial_mode = renamer._buffers[window_id].opts and renamer._buffers[window_id].opts.initial_mode
-        if initial_mode and not string.match(initial_mode, 'i') then
-            vim.api.nvim_command [[stopinsert]]
-        end
         renamer.on_close(window_id)
-
         renamer._lsp_rename(new_word)
     end
 end
@@ -171,13 +165,19 @@ function renamer.on_close(window_id)
         end
     end
 
-    local opts = renamer._buffers and renamer._buffers[window_id]
-    local border_win_id = opts and opts.border_opts and opts.border_opts.win_id
+    local settings = renamer._buffers and renamer._buffers[window_id]
+    local border_win_id = settings and settings.border_opts and settings.border_opts.win_id
+    local initial_mode = settings and settings.opts and settings.opts.initial_mode
+
     delete_window(window_id)
     log.fmt_info('Deleted window: "%s".', window_id)
     delete_window(border_win_id)
     log.fmt_info('Deleted window: "%s" (border).', border_win_id)
+
     renamer._clear_references()
+    if initial_mode and not string.match(initial_mode, 'i') then
+        vim.api.nvim_command [[stopinsert]]
+    end
 end
 
 function renamer._get_cursor()

lua/tests/renamer_close_spec.lua
@@ -116,66 +116,6 @@ describe('renamer', function()
             on_close.revert(on_close)
             lsp_rename.revert(lsp_rename)
         end)
-
-        it('should stay in insert mode if opened in it', function()
-            local expected_win_id, expected_buf_id = 123, 321
-            local api_mock = mock(vim.api, true)
-            api_mock.nvim_win_get_buf.returns(expected_buf_id)
-            api_mock.nvim_buf_get_lines.returns {}
-            api_mock.nvim_command.returns()
-            local on_close = stub(renamer, 'on_close').returns()
-            local lsp_rename = stub(renamer, '_lsp_rename').returns()
-            renamer._buffers[expected_win_id] = { opts = { initial_mode = 'i' } }
-
-            renamer.on_submit(expected_win_id)
-
-            assert.spy(api_mock.nvim_command).called_less_than(1)
-            assert.spy(api_mock.nvim_win_get_buf).was_called_with(expected_win_id)
-            assert.spy(api_mock.nvim_buf_get_lines).was_called_with(expected_buf_id, -2, -1, false)
-            mock.revert(api_mock)
-            on_close.revert(on_close)
-            lsp_rename.revert(lsp_rename)
-        end)
-
-        it('should exit insert mode if not opened in it (initial mode is `normal`)', function()
-            local expected_win_id, expected_buf_id = 123, 321
-            local api_mock = mock(vim.api, true)
-            api_mock.nvim_win_get_buf.returns(expected_buf_id)
-            api_mock.nvim_buf_get_lines.returns {}
-            api_mock.nvim_command.returns()
-            local on_close = stub(renamer, 'on_close').returns()
-            local lsp_rename = stub(renamer, '_lsp_rename').returns()
-            renamer._buffers[expected_win_id] = { opts = { initial_mode = 'n' } }
-
-            renamer.on_submit(expected_win_id)
-
-            assert.spy(api_mock.nvim_command).was_called_with [[stopinsert]]
-            assert.spy(api_mock.nvim_win_get_buf).was_called_with(expected_win_id)
-            assert.spy(api_mock.nvim_buf_get_lines).was_called_with(expected_buf_id, -2, -1, false)
-            mock.revert(api_mock)
-            on_close.revert(on_close)
-            lsp_rename.revert(lsp_rename)
-        end)
-
-        it('should exit insert mode if not opened in it (initial mode is `visual`)', function()
-            local expected_win_id, expected_buf_id = 123, 321
-            local api_mock = mock(vim.api, true)
-            api_mock.nvim_win_get_buf.returns(expected_buf_id)
-            api_mock.nvim_buf_get_lines.returns {}
-            api_mock.nvim_command.returns()
-            local on_close = stub(renamer, 'on_close').returns()
-            local lsp_rename = stub(renamer, '_lsp_rename').returns()
-            renamer._buffers[expected_win_id] = { opts = { initial_mode = 'v' } }
-
-            renamer.on_submit(expected_win_id)
-
-            assert.spy(api_mock.nvim_command).was_called_with [[stopinsert]]
-            assert.spy(api_mock.nvim_win_get_buf).was_called_with(expected_win_id)
-            assert.spy(api_mock.nvim_buf_get_lines).was_called_with(expected_buf_id, -2, -1, false)
-            mock.revert(api_mock)
-            on_close.revert(on_close)
-            lsp_rename.revert(lsp_rename)
-        end)
     end)
 
     describe('on_close', function()
@@ -379,6 +319,49 @@ describe('renamer', function()
             renamer.on_close(expected_win_id)
 
             assert.spy(clear_references).was_called()
+            mock.revert(api_mock)
+            clear_references.revert(clear_references)
+        end)
+
+        it('should stay in insert mode if opened in it', function()
+            local expected_win_id = 123
+            local api_mock = mock(vim.api, true)
+            api_mock.nvim_win_is_valid.returns(false)
+            local clear_references = stub(renamer, '_clear_references').returns()
+            renamer._buffers[expected_win_id] = { opts = { initial_mode = 'i' } }
+
+            renamer.on_close(expected_win_id)
+
+            assert.spy(api_mock.nvim_command).called_less_than(1)
+            mock.revert(api_mock)
+            clear_references.revert(clear_references)
+        end)
+
+        it('should exit insert mode if not opened in it (initial mode is `normal`)', function()
+            local expected_win_id = 123
+            local api_mock = mock(vim.api, true)
+            api_mock.nvim_win_is_valid.returns(false)
+            local clear_references = stub(renamer, '_clear_references').returns()
+            renamer._buffers[expected_win_id] = { opts = { initial_mode = 'n' } }
+
+            renamer.on_close(expected_win_id)
+
+            assert.spy(api_mock.nvim_command).was_called_with [[stopinsert]]
+            mock.revert(api_mock)
+            clear_references.revert(clear_references)
+        end)
+
+        it('should exit insert mode if not opened in it (initial mode is `visual`)', function()
+            local expected_win_id = 123
+            local api_mock = mock(vim.api, true)
+            api_mock.nvim_win_is_valid.returns(false)
+            api_mock.nvim_command.returns()
+            local clear_references = stub(renamer, '_clear_references').returns()
+            renamer._buffers[expected_win_id] = { opts = { initial_mode = 'v' } }
+
+            renamer.on_close(expected_win_id)
+
+            assert.spy(api_mock.nvim_command).was_called_with [[stopinsert]]
             mock.revert(api_mock)
             clear_references.revert(clear_references)
         end)

commit cdcdb1760822240d9a93cd0f08f10f39d190c7c4

fix: cursor placement when finishing `rename()`
cdb40c6 (GH-33) introduced a bug which made the cursor go 1 character
back when finishing the `rename()` call (by cancellation or submission).

To fix this, the popup options also contain various information
regarding the starting position (where the word starts and the cursor
position at the moment of the call). This makes is easy to update
`on_close()` to set the cursor at the initial position on cancellation
(behaviour toggled by a new parameter).

This commit also makes it so as, upon submission of the rename, the
cursor is placed at the end of the new word, by copying the current
implementation of `vim.lsp.buf.rename()`. The new `_lsp_rename` method
makes LSP requests in the same way Neovim implements the rename feature
and uses a callback to asynchronously set the cursor position.

Fixes: GH-34
Signed-off-by: Filip Dutescu <filip.dutescu@gmail.com>

lua/renamer/init.lua
@@ -2,6 +2,7 @@ local log = require('plenary.log').new {
     plugin = 'renamer',
     level = 'warn',
 }
+local lsp_utils = require 'vim.lsp.util'
 local popup = require 'plenary.popup'
 local utils = require 'renamer.utils'
 local mappings = require 'renamer.mappings'
@@ -110,6 +111,11 @@ function renamer.rename()
         cursor_line = true,
         enter = true,
         initial_mode = vim.api.nvim_get_mode().mode,
+        initial_pos = {
+            word_start = word_start,
+            col = col,
+            line = line,
+        },
     }
     local prompt_win_id, prompt_opts = popup.create(cword, popup_opts)
 
@@ -129,54 +135,39 @@ end
 
 function renamer.on_submit(window_id)
     if window_id and renamer._buffers and renamer._buffers[window_id] then
+        local opts = renamer._buffers[window_id].opts
+        local pos = opts and opts.initial_pos
         local buf_id = vim.api.nvim_win_get_buf(window_id)
         local new_word = vim.api.nvim_buf_get_lines(buf_id, -2, -1, false)[1]
         log.fmt_info('Submitted word: "%s".', new_word)
 
         renamer._delete_autocmds()
-        renamer.on_close(window_id)
-        renamer._lsp_rename(new_word)
+        renamer.on_close(window_id, false)
+        renamer._lsp_rename(new_word, pos)
     end
 end
 
-function renamer.on_close(window_id)
-    local delete_window = function(win_id)
-        if win_id and vim.api.nvim_win_is_valid(win_id) then
-            local buf_id = vim.api.nvim_win_get_buf(win_id)
-            if
-                buf_id
-                and vim.api.nvim_buf_is_valid(buf_id)
-                and not vim.api.nvim_buf_get_option(buf_id, 'buflisted')
-            then
-                vim.api.nvim_command(string.format('silent! bdelete! %s', buf_id))
-            end
-
-            if vim.api.nvim_win_is_valid(win_id) then
-                vim.api.nvim_win_close(win_id, true)
-                if not pcall(vim.api.nvim_win_close, win_id, true) then
-                    log.trace('Failed to close window: rename_prompt_win/' .. win_id)
-                end
-            end
-        end
-
-        if win_id and renamer._buffers and renamer._buffers[win_id] then
-            renamer._buffers[win_id] = nil
-            renamer._delete_autocmds()
-        end
+function renamer.on_close(window_id, should_set_cursor_pos)
+    if should_set_cursor_pos == nil then
+        should_set_cursor_pos = true
     end
-
     local settings = renamer._buffers and renamer._buffers[window_id]
     local border_win_id = settings and settings.border_opts and settings.border_opts.win_id
     local initial_mode = settings and settings.opts and settings.opts.initial_mode
+    local pos = settings and settings.opts and settings.opts.initial_pos
 
-    delete_window(window_id)
+    renamer._delete_window(window_id)
     log.fmt_info('Deleted window: "%s".', window_id)
-    delete_window(border_win_id)
+    renamer._delete_window(border_win_id)
     log.fmt_info('Deleted window: "%s" (border).', border_win_id)
 
     renamer._clear_references()
     if initial_mode and not string.match(initial_mode, 'i') then
         vim.api.nvim_command [[stopinsert]]
+    end
+
+    if should_set_cursor_pos and pos then
+        vim.api.nvim_win_set_cursor(0, { pos.line, pos.col + 1 })
     end
 end
 
@@ -285,8 +276,35 @@ function renamer._delete_autocmds()
 end
 
 -- Since there is no way to mock `vim.lsp.buf.rename`, this function is used as a replacement.
-function renamer._lsp_rename(word)
-    vim.lsp.buf.rename(word)
+function renamer._lsp_rename(word, pos)
+    local params = renamer._make_position_params()
+
+    renamer._buf_request(0, 'textDocument/prepareRename', params, function(prep_err, prep_resp)
+        if prep_err == nil and prep_resp == nil then
+            log.warn 'Nothing to rename.'
+            return
+        end
+        params.newName = word
+
+        renamer._buf_request(0, 'textDocument/rename', params, function(err, resp)
+            if err then
+                log.error(err)
+                return
+            end
+
+            if not resp then
+                log.warn 'LSP response is nil.'
+                return
+            end
+
+            lsp_utils.apply_workspace_edit(resp)
+
+            if pos then
+                local col = pos.word_start + #word - 1
+                vim.api.nvim_win_set_cursor(0, { pos.line, col })
+            end
+        end)
+    end)
 end
 
 -- Since there is no way to mock `vim.lsp.buf.document_highlight`, this function is used as a replacement.
@@ -294,9 +312,40 @@ function renamer._document_highlight()
     vim.lsp.buf.document_highlight()
 end
 
+function renamer._delete_window(win_id)
+    if win_id and vim.api.nvim_win_is_valid(win_id) then
+        local buf_id = vim.api.nvim_win_get_buf(win_id)
+        if buf_id and vim.api.nvim_buf_is_valid(buf_id) and not vim.api.nvim_buf_get_option(buf_id, 'buflisted') then
+            vim.api.nvim_command(string.format('silent! bdelete! %s', buf_id))
+        end
+
+        if vim.api.nvim_win_is_valid(win_id) then
+            vim.api.nvim_win_close(win_id, true)
+            if not pcall(vim.api.nvim_win_close, win_id, true) then
+                log.trace('Failed to close window: rename_prompt_win/' .. win_id)
+            end
+        end
+    end
+
+    if win_id and renamer._buffers and renamer._buffers[win_id] then
+        renamer._buffers[win_id] = nil
+        renamer._delete_autocmds()
+    end
+end
+
 -- Since there is no way to mock `vim.lsp.buf.clear_references`, this function is used as a replacement.
 function renamer._clear_references()
     vim.lsp.buf.clear_references()
+end
+
+-- Since there is no way to mock `vim.lsp.buf_request`, this function is used as a replacement
+function renamer._make_position_params()
+    return lsp_utils.make_position_params()
+end
+
+-- Since there is no way to mock `vim.lsp.buf_request`, this function is used as a replacement
+function renamer._buf_request(buf_id, method, params, handler)
+    vim.lsp.buf_request(buf_id, method, params, handler)
 end
 
 return renamer

lua/tests/renamer_close_spec.lua
@@ -87,7 +87,7 @@ describe('renamer', function()
 
             renamer.on_submit(expected_win_id)
 
-            assert.spy(renamer.on_close).was_called_with(expected_win_id)
+            assert.spy(renamer.on_close).was_called_with(expected_win_id, false)
             assert.spy(api_mock.nvim_win_get_buf).was_called_with(expected_win_id)
             assert.spy(api_mock.nvim_buf_get_lines).was_called_with(expected_buf_id, -2, -1, false)
             mock.revert(api_mock)
@@ -104,17 +104,47 @@ describe('renamer', function()
             api_mock.nvim_command.returns()
             local on_close = stub(renamer, 'on_close').returns()
             local lsp_rename = stub(renamer, '_lsp_rename').returns()
-            renamer._buffers[expected_win_id] = {}
+            local expected_pos = { word_start = 1, line = 1, col = 1 }
+            renamer._buffers[expected_win_id] = { opts = { initial_pos = expected_pos } }
 
             renamer.on_submit(expected_win_id)
 
-            assert.spy(lsp_rename).was_called_with(expected_content)
-            assert.spy(on_close).was_called_with(expected_win_id)
+            assert.spy(lsp_rename).was_called_with(expected_content, expected_pos)
+            assert.spy(on_close).was_called_with(expected_win_id, false)
             assert.spy(api_mock.nvim_win_get_buf).was_called_with(expected_win_id)
             assert.spy(api_mock.nvim_buf_get_lines).was_called_with(expected_buf_id, -2, -1, false)
             mock.revert(api_mock)
             on_close.revert(on_close)
             lsp_rename.revert(lsp_rename)
+        end)
+
+        it('should set cursor after the end of the new word', function()
+            local expected_win_id, expected_buf_id = 123, 321
+            local expected_content = 'test'
+            local api_mock = mock(vim.api, true)
+            api_mock.nvim_win_get_buf.returns(expected_buf_id)
+            api_mock.nvim_buf_get_lines.returns { expected_content }
+            api_mock.nvim_command.returns()
+            api_mock.nvim_win_set_cursor.returns()
+            local on_close = stub(renamer, 'on_close').returns()
+            local expected_pos = { word_start = 1, line = 1, col = 1 }
+            local expected_params = {}
+            local make_params = stub(renamer, '_make_position_params').returns(expected_params)
+            local buf_request = stub(renamer, '_buf_request').invokes(function()
+                api_mock.nvim_win_set_cursor(0, { expected_pos.line, expected_pos.col })
+            end)
+            renamer._buffers[expected_win_id] = { opts = { initial_pos = expected_pos } }
+
+            renamer.on_submit(expected_win_id)
+
+            assert.spy(buf_request).was_called()
+            assert.spy(on_close).was_called_with(expected_win_id, false)
+            assert.spy(api_mock.nvim_win_get_buf).was_called_with(expected_win_id)
+            assert.spy(api_mock.nvim_buf_get_lines).was_called_with(expected_buf_id, -2, -1, false)
+            mock.revert(api_mock)
+            buf_request.revert(buf_request)
+            on_close.revert(on_close)
+            make_params.revert(make_params)
         end)
     end)
 
@@ -362,6 +392,23 @@ describe('renamer', function()
             renamer.on_close(expected_win_id)
 
             assert.spy(api_mock.nvim_command).was_called_with [[stopinsert]]
+            mock.revert(api_mock)
+            clear_references.revert(clear_references)
+        end)
+
+        it('should set cursor in the initial position (when `rename()` is cancelled)', function()
+            local expected_win_id = 123
+            local api_mock = mock(vim.api, true)
+            api_mock.nvim_win_is_valid.returns(false)
+            api_mock.nvim_command.returns()
+            api_mock.nvim_win_set_cursor.returns()
+            local clear_references = stub(renamer, '_clear_references').returns()
+            local expected_pos = { col = 1, line = 1 }
+            renamer._buffers[expected_win_id] = { opts = { initial_pos = expected_pos } }
+
+            renamer.on_close(expected_win_id)
+
+            assert.spy(api_mock.nvim_win_set_cursor).was_called_with(0, { expected_pos.line, expected_pos.col + 1 })
             mock.revert(api_mock)
             clear_references.revert(clear_references)
         end)

lua/tests/renamer_rename_spec.lua
@@ -89,8 +89,8 @@ describe('renamer', function()
         end)
 
         it('should return the buffer ID and the popup options', function()
-            local expected_col_no = 2
-            local expected_line_no = 2
+            local expected_col_no, expected_line_no = 2, 2
+            local word_start = 1
             local expected_opts = {
                 title = renamer.title,
                 padding = renamer.padding,
@@ -105,11 +105,16 @@ describe('renamer', function()
                 cursor_line = true,
                 enter = true,
                 initial_mode = 'test',
+                initial_pos = {
+                    word_start = word_start,
+                    col = expected_col_no,
+                    line = expected_line_no,
+                },
             }
             local expected_border_opts = {}
             local expected_buf_id = 123
             local api_mock = mock(vim.api, true)
-            api_mock.nvim_win_get_cursor.returns { 1, expected_col_no }
+            api_mock.nvim_win_get_cursor.returns { expected_line_no, expected_col_no }
             api_mock.nvim_command.returns()
             api_mock.nvim_buf_line_count.returns(expected_line_no + 5)
             api_mock.nvim_get_mode.returns { mode = expected_opts.initial_mode }